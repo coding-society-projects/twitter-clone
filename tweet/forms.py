2f9afdfa434acaf688bb906c1c81fd196565f1f3
gprok <gprok60@gmail.com>
2021-03-27 17:02:09 +0200

    styled feed and profile

--- tweet/forms.py
+++ tweet/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class TweetForm(forms.Form):
-    tweet = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "What's happening?", 'rows':4, 'cols':45}),
+    tweet = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "What's happening?",
+                                                         'class': 'tweet_form', 'rows':3, 'cols':63}),
                             max_length=140, required=True, label=False)
